6b38435d440f5f5686a8b0c7ff8e161a69bcae9c
technote <>
2020-08-03 14:15:23 +0000

    開発環境調整

--- assets/webpack.base.config.js
+++ assets/webpack.base.config.js
@@ -1,56 +1,56 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require( 'path' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 
 module.exports = {
     entry: {
-        index: "./src/js/app.js",
+        index: './src/js/app.js'
     },
     output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "../js"),
+        filename: '[name].bundle.js',
+        path: path.resolve( __dirname, '../js' )
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
-                include: path.resolve(__dirname, "src/js"),
+                include: path.resolve( __dirname, 'src/js' ),
                 use: [
                     {
-                        loader: "babel-loader",
+                        loader: 'babel-loader',
                         options: {
                             presets: [
                                 [
-                                    "@babel/env",
+                                    '@babel/env',
                                     {
                                         targets: {
-                                            ie: "11",
+                                            ie: '11'
                                         },
-                                        useBuiltIns: "usage",
+                                        useBuiltIns: 'usage',
                                         corejs: 3,
-                                        debug: true,
-                                    },
-                                ],
-                            ],
-                        },
-                    },
-                ],
+                                        debug: true
+                                    }
+                                ]
+                            ]
+                        }
+                    }
+                ]
             },
             {
                 test: /\.scss$/,
-                include: path.resolve(__dirname, "src/sass"),
+                include: path.resolve( __dirname, 'src/sass' ),
                 use: [
                     MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader",
-                    "import-glob-loader",
-                ],
+                    'css-loader',
+                    'sass-loader',
+                    'import-glob-loader'
+                ]
             }
-        ],
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
-          filename: '../style1.css',
-          ignoreOrder: true,
+          filename: '../assets/public/style.css',
+          ignoreOrder: true
         })
       ]
 };
