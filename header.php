bbd7d1edea808fe1b10a3e0695dc9ffc6e945288
technote <>
2020-08-17 14:33:28 +0000

    meta編集

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="format-detection" content="telephone=no">
 	<link rel="shortcut icon" href="/common/images/icons/favicon.ico">
 <?php wp_head(); ?>
 </head>
